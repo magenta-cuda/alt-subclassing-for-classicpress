a59a67063c0a7918c50dba19656ee7eee5408af9
Omega Turtle <magenta.cuda@yahoo.com>
2021-07-16 09:50:40 -1000

    prettify

--- alt-subclassing-for-classicpress.php
+++ alt-subclassing-for-classicpress.php
@@ -73,40 +73,40 @@ class Alpha {
 add_filter( 'alpha_beta', function( $beta ) {
     error_log( 'installing beta1():$beta = ' . print_r( $beta, true ) );
     $inner_beta1 = $beta;
-    return function( $_this1, $gamma1 )  use ( $inner_beta1 ) {
-        error_log( 'beta1():$gamma1 = ' . $gamma1 );
+    return function( $_this1, $gamma )  use ( $inner_beta1 ) {
+        $gamma1 = $gamma + 100;
+        error_log( 'beta1():$gamma: ' . $gamma . ' -> ' . $gamma1 );
         ++$_this1->epsilon;
         error_log( 'beta1():$_this1 = ' . print_r( $_this1, true ) );
-        $gamma1 += 100;
         error_log( 'beta1():$inner_beta1 = ' . print_r( $inner_beta1, true ) );
         if ( is_array( $inner_beta1 ) ) {
             $result = call_user_func( $inner_beta1,         $gamma1 );
         } else {
             $result = call_user_func( $inner_beta1, $_this1, $gamma1 );
         }
-        $result += 1000;
-        error_log( 'beta1():$result = ' . $result );
-        return $result;
+        $result1 = $result + 1000;
+        error_log( 'beta1():$result: ' . $result . ' -> ' . $result1 );
+        return $result1;
     };
 }, 100 );
 
 add_filter( 'alpha_beta', function( $beta ) {
     error_log( 'installing beta2():$beta = ' . print_r( $beta, true ) );
     $inner_beta2 = $beta;
-    return function( $_this2, $gamma2 ) use ( $inner_beta2 ) {
-        error_log( 'beta2():$gamma2 = ' . $gamma2 );
+    return function( $_this2, $gamma ) use ( $inner_beta2 ) {
+        $gamma2 = $gamma + 10;
+        error_log( 'beta2():$gamma: ' . $gamma . ' -> ' . $gamma2 );
         $_this2->epsilon += 1;
         error_log( 'beta2():$_this2 = ' . print_r( $_this2, true ) );
-        $gamma2 += 10;
         error_log( 'beta2():$inner_beta2 = ' . print_r( $inner_beta2, true ) );
         if ( is_array( $inner_beta2 ) ) {
             $result = call_user_func( $inner_beta2,         $gamma2 );
         } else {
             $result = call_user_func( $inner_beta2, $_this2, $gamma2 );
         }
-        $result += 10000;
-        error_log( 'beta2():$result = ' . $result );
-        return $result;
+        $result2 = $result + 10000;
+        error_log( 'beta2():$result: ' . $result . ' -> ' . $result2 );
+        return $result2;
     };
 }, 200 );
 
